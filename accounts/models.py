65e910e0462d60182b58a3a0149b3d4ad11bffea
solar <solarishibashi@gmail.com>
2026-09-19 12:19:40 -0400

    created models, started urls and views in accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Skill model will make it easier to filter search
+class Skill(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+
+
+class JobSeeker(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    headline = models.TextField()
+    skills = models.ManyToManyField(Skill, blank=True)
+    education = models.TextField()
+    #will add more properties later
+
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
 from . import views
+urlpatterns = [
+    path('signup', views.signup, name='accounts.signup'),
+    path('login', views.login, name='accounts.login'),
+    path('profile', views.profile, name='accounts.profile'),
+]
